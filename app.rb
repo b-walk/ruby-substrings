71625eaba050aea29626ba918aa8a64dc1ac34a4
Blake <blameswalker@gmail.com>
2023-04-11 18:09:15 -0700

    Implement substrings method; allow for user input

--- app.rb
+++ app.rb
@@ -1,1 +1,17 @@
+require 'pry-byebug'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+    string.downcase!
+    binding.pry
+    tallies_hash = Hash.new(0)
+    dictionary.each do |word|
+        tallies_hash[word] += string.scan(/(?=#{word})/).count
+    end
+    tallies_hash
+end
+
+puts "Enter a string:"
+string = gets.chomp
+
+puts substrings(string, dictionary)
